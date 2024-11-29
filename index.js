bfe3b9c36a6973301fd3ff1c110fb1d6400c0a47
Nandiniatri <atrinandini8@gmail.com>
2024-11-29 14:38:47 +0530

    Initial commit with index.js and package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("This is Nandini Atri Developer, creator of the 'my-npm-package'");
+
+export function greet(name) {
+  return `Hello, ${name}! This package was created by Nandini Atri.`;
+  
+}
